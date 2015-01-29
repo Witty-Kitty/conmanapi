95ecd1e4f335bd02c12d79214767625d1b3c629f
Kathleen Siminyu <kathleensiminyu@gmail.com>
2015-01-29 15:40:15 +0300

    Create New Contact Authentication

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -32,63 +32,77 @@ class ContactsController extends \BaseController
      */
     public function store()
     {
+        $validator = Validator::make(Input::all(),
+            array(
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'email_address' => 'required|email',
+                'twitter' => 'required',
+            ));
 
+        if ($validator->fails()) {
+            return Response::json([
+                'data' => $validator->messages()
+            ], 403);
+        } else {
 
             $contact = Contact::create(Input::all());
 
             return $contact;
 
         }
-    
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-
-      return  Contact::find($id);
-
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        $contact = Contact::findOrFail($id);
-
-        $input = array_filter(Input::only(['first_name', 'last_name', 'email_address', 'twitter']));
-
-
-        foreach($input as $attr => $value)
+        /**
+         * Display the specified resource.
+         *
+         * @param  int $id
+         * @return Response
+         */
+        public
+        function show($id)
         {
 
-            $contact->$attr = $value;
+            return Contact::find($id);
+
         }
 
-        $contact->save();
 
-        return $contact;
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param  int $id
+         * @return Response
+         */
+        public
+        function update($id)
+        {
+            $contact = Contact::findOrFail($id);
 
-    }
+            $input = array_filter(Input::only(['first_name', 'last_name', 'email_address', 'twitter']));
 
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
+            foreach ($input as $attr => $value) {
+
+                $contact->$attr = $value;
+            }
+
+            $contact->save();
+
+            return $contact;
+
+        }
+
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param  int $id
+         * @return Response
+         */
+        public
+        function destroy($id)
+        {
 
             $contact = Contact::findOrFail($id);
 
@@ -96,21 +110,23 @@ class ContactsController extends \BaseController
 
             return "Contact has been permanently deleted";
 
+        }
+
+
+        public
+        function archive($id)
+        {
+
+            Contact::findOrFail($id)->delete();
+
+            return "Contact has been SoftDeleted";
+
+        }
+
+        public
+        function restore($id)
+        {
+            Contact::withTrashed()->where('id', $id)->restore();
+        }
+
     }
-
-
-    public function archive($id)
-    {
-
-        Contact::findOrFail($id)->delete();
-
-        return "Contact has been SoftDeleted";
-
-    }
-
-    public function restore($id)
-    {
-        Contact::withTrashed()->where('id', $id)->restore();
-    }
-
-}
